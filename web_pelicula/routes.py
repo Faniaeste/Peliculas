97cacecce01c250169e7cbead1ce8a5dbeae6ba7
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-02-22 17:34:37 +0100

    He creado la pagina results para que se muestren los resultados, he creado styles para tenerlo concentrado, y he ido conectandolo con base e index, si el cliente escribe y se equivoca no se borran los datos, el boton de volver al buscador redirige a la pagina principal.

--- web_pelicula/routes.py
+++ web_pelicula/routes.py
@@ -1,6 +1,25 @@
 from web_pelicula import app
-from flask import render_template,redirect,request,flash
+from flask import render_template,request
+from config import SECRET_KEY
+import requests
 
 @app.route("/")
 def index():
     return render_template("index.html", dataForm={}, button="Buscar película")
+
+@app.route("/buscar")
+def buscar():
+    titulo = request.values.get('t')
+    anno = request.values.get('y')
+    #Aquí guardo lo escrito por el cliente
+    datos = {'titulo':titulo, 'anno':anno}
+    url = f"http://www.omdbapi.com/?apikey={SECRET_KEY}&t={titulo}&y={anno}"
+
+    respuesta = requests.get(url)
+    pelicula = respuesta.json()
+
+    if pelicula.get('Response') == 'True':
+        return render_template("results.html", dataForm=pelicula, button="Buscar película")
+    else:
+        #Aquí en dataform dejo los datos ya escritos
+        return render_template("index.html", dataForm=datos, error="No encontramos la película", button="Buscar película")
