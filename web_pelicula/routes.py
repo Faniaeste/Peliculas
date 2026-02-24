3211e1a3cd590bd8546dc78017da84c37a569fe6
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-02-24 20:53:42 +0100

    He creado la base de datos de sqlite y la he conectado con results y routes, al darle a guardar comentario me sale error 404, he guardado el formulario hecho en el browser en create.sqlite, pero lo tengo que modificar porque me olvidé un campo que agregué después, la parte guardado de comentario no funciona todavía.

--- web_pelicula/routes.py
+++ web_pelicula/routes.py
@@ -1,7 +1,9 @@
 from web_pelicula import app
-from flask import render_template,request
+from flask import render_template,request,redirect,url_for
 #Llamo a las dos funciones par poderlas utilizar en las rutas
 from .services.omdb_service import obtener_peliculas,obtener_detalles_pelicula
+#Esto crea la conexión entre la base de datos y la web
+import sqlite3
 
 #Pantalla principal
 @app.route("/")
@@ -28,4 +30,27 @@ def buscar():
 def detalle(id):
     detalles = obtener_detalles_pelicula(id)
     return render_template("results.html", dataForm=detalles)
+
+#Aquí guardamos los datos y comentarios de los clientes cogidos en results.html
+@app.route("/guardar_comentario/<id>", methods=['POST'])
+def guardar_comentario(id):
+    nombre = request.form.get('nombre')
+    comentario = request.form.get('comentario')
+
+    conexion = sqlite3.connect('peliculas.db')
+    #Aquí llamo a la funcion
+    cursor = conexion.cursor()
+    #Aquí le digo que guarde y donde
+    #Los??? son un metodo de seguridad
+    cursor.execute('''
+        INSERT INTO comentarios (id, nombre, comentario,)
+        VALUES (?, ?, ?, )''', (id, nombre, comentario,))
+    
+    #commit guarda en la tabla lo comentado    
+    conexion.commit()
+    #Cierro la conexión para que no se pierdan los datos guardados
+    conexion.close()
+
+    #url_for busca la dirección results  y le dice que busque ese id
+    return redirect(url_for('guardar_comentario', id=id))
 
