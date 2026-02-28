4c18409e47e3cb209afa9682a1a463393b87e056
Estefania Sanchez Sanchez <faniaeste92@gmail.com>
2026-02-28 21:29:54 +0100

    He desahogado routes y mandando funciones a models, he agregado la fecha y por ello he tenido que volver hacer la tabla., guarda los comenatrios correctamente. ¡FUNCIONA TODO!

--- web_pelicula/models.py
+++ web_pelicula/models.py
@@ -7,10 +7,27 @@ def select_comentarios(imdb_id):
     #Llamamos al cursor para buscar
     cursor = conexion.cursor()
     #Traeme el nombre y el comentario que esten relacionados con la id
-    cursor.execute("SELECT nombre, comentario FROM comentarios WHERE imdb_id = ?", (imdb_id,))
+    cursor.execute("SELECT nombre, comentario, strftime('%d/%m/%Y %H:%M',fecha) FROM comentarios WHERE imdb_id = ?", (imdb_id,))
     #Aquí guardamos todos los datos que el cursor a encontrado
     resultado = cursor.fetchall()
     #cuando lo encuentres cierra la conexión
     conexion.close()
     #Devuelveme lo encontrado
     return resultado
+
+#Función para guardar un nuevo comentario
+def insertar_comentario(imdb_id, nombre, comentario):
+    conexion = sqlite3.connect('peliculas.db')
+    cursor = conexion.cursor()
+    #Aquí le digo que guarde y donde
+    #Los??? son un metodo de seguridad 
+    #Las """ son para que no sea tan largo el codigo y hacer varias lineas
+    cursor.execute('''
+        INSERT INTO comentarios (imdb_id, nombre, comentario)
+        VALUES (?, ?, ? )
+    ''', (imdb_id, nombre, comentario))
+    
+    #commit guarda en la tabla lo comentado    
+    conexion.commit()
+    #Cierro la conexión para que no se pierdan los datos guardados
+    conexion.close()
--- web_pelicula/routes.py
+++ web_pelicula/routes.py
@@ -2,9 +2,7 @@ from web_pelicula import app
 from flask import render_template,request,redirect,url_for
 #Llamo a las dos funciones par poderlas utilizar en las rutas
 from web_pelicula.services.omdb_service import obtener_peliculas,obtener_detalles_pelicula
-#Esto crea la conexión entre la base de datos y la web
-import sqlite3
-from web_pelicula.models import select_comentarios
+from web_pelicula import models
 
 #Pantalla principal
 @app.route("/")
@@ -32,33 +30,20 @@ def detalle(id):
     #Busca los detalles de la pelicula llamando a la función
     detalles = obtener_detalles_pelicula(id)
     #Busca los comentarios relacionados con ese id llamando a la función
-    comentarios = select_comentarios(id)
+    comentarios = models.select_comentarios(id)
     #Lo muestro en la pantalla
     return render_template("results.html", dataForm=detalles, comentarios=comentarios)
 
 #Aquí guardamos los datos y comentarios de los clientes cogidos en results.html
 @app.route("/guardar_comentario/<imdb_id>", methods=['POST'])
 def guardar_comentario(imdb_id):
+    #Sacamos los datos del formulario (request.form)
     nombre = request.form.get('nombre')
     comentario = request.form.get('comentario')
 
-    conexion = sqlite3.connect('peliculas.db')
-    #Aquí llamo a la funcion
-    cursor = conexion.cursor()
-    #Aquí le digo que guarde y donde
-    #Los??? son un metodo de seguridad 
-    #Las """ son para que no sea tan largo el codigo y hacer varias lineas
-    cursor.execute('''
-        INSERT INTO comentarios (imdb_id, nombre, comentario)
-        VALUES (?, ?, ? )
-    ''', (imdb_id, nombre, comentario))
-    
-    #commit guarda en la tabla lo comentado    
-    conexion.commit()
-    #Cierro la conexión para que no se pierdan los datos guardados
-    conexion.close()
+    #Usamos la función que acabamos de crear en models
+    models.insertar_comentario(imdb_id, nombre, comentario)
 
-    #url_for busca la dirección results  y le dice que busque ese id
-    #Guarda lo escrito y redirige la pagina a detalle
+    #Volvemos a la página del detalle de la película
     return redirect(url_for('detalle', id=imdb_id))
 
